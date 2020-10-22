3615345b3eaea42465f86d7db3b9a98a7cae3997
aditya-vijaykumar <aditya.tarus@gmail.com>
2020-10-23 03:06:19 +0530

    explore

--- src/views/Explore.vue
+++ src/views/Explore.vue
@@ -1,42 +1,82 @@
 <template>
-  <tabs fill class="flex-column flex-md-row">
-    <card shadow>
-        <tab-pane>
-            <span v-slot:="title">
-                <i class="ni ni-cloud-upload-96">
-                ENS
-            </span>
-            <p class="description">Raw denim you probably haven't heard of them jean shorts
-                Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache
-                cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro
-                keffiyeh dreamcatcher synth.</p>
-        </tab-pane>
+  <section class="section section-shaped section-lg my-0">
+    <div class="shape shape-style-1 bg-gradient-default">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    <div class="container pt-lg-md">
+      <div class="row justify-content-center">
+        <div class="col-lg-9">
+          <card
+            type="secondary"
+            shadow
+            header-classes="bg-white pb-5"
+            body-classes="px-lg-5 py-lg-5"
+            class="border-0"
+          >
+            <div class="text-center mb-3">
+              <h3>Enter did to resolve for profile</h3>
+            </div>
 
-        <tab-pane title="Profile">
-            <span v-slot:="title">
-                <i class="ni ni-bell-55 mr-2">
-                DID
-            </span>
-            <p class="description">Cosby sweater eu banh mi, qui irure terry richardson ex
-                squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan
-                american apparel, butcher voluptate nisi qui.</p>
-        </tab-pane>
+            <template>
+              <form role="form">
+                <base-input
+                  alternative
+                  class="mb-3"
+                  placeholder="did"
+                  addon-left-icon="ni ni-hat-3"
+                >
+                </base-input>
 
-        <tab-pane>
-             <span v-slot:="title">
-                <i class="ni ni-calendar-grid-58">
-               Eth address
-              </span>
-            <p class="description">Raw denim you probably haven't heard of them jean shorts
-                Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache
-                cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro
-                keffiyeh dreamcatcher synth.</p>
-        </tab-pane>
-    </card>
-</tabs>
+                <div class="text-center">
+                  <base-button type="primary" class="my-4">Resolve</base-button>
+                </div>
+              </form>
+            </template>
+          </card>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
-export default {};
+import { IDXWeb } from "@ceramicstudio/idx-web";
+import CeramicClient from "@ceramicnetwork/ceramic-http-client";
+import { ThreeIdConnect } from "../../node_modules/3id-connect/src/index";
+export default {
+  name: "explore",
+  data: () => {
+    return {};
+  },
+  methods: {
+    async fetch() {
+      const THREEID_CONNECT_URL = "https://3idconnect.org/index.html";
+      const DEFAULT_API_URL = "https://ceramic.3boxlabs.com";
+      const API_URL = "http://localhost:7007";
+      const threeIdConnect = new ThreeIdConnect(THREEID_CONNECT_URL);
+      const ceramic = new CeramicClient(DEFAULT_API_URL);
+      const { definitions } = await publishIDXConfig(ceramic);
+      const appDefinitions = {
+        profile: definitions.basicProfile,
+      };
+      const bobClient = new IDXWeb({
+        ceramic,
+        definitions: appDefinitions,
+        connect: threeIdConnect,
+      });
+      const MyProfile = await bobClient.get("profile", idx.id);
+
+      console.log("My Profile: ");
+      console.dir(MyProfile);
+    },
+  },
+};
 </script>
 <style>
 </style>
