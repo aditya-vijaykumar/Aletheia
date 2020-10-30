bb4a90ece74ea43bfc1ba964fa3621d1802cb7e0
aditya-vijaykumar <aditya.tarus@gmail.com>
2020-10-30 20:03:29 +0530

    Repo clean up

--- src/router.js
+++ src/router.js
@@ -2,13 +2,8 @@ import Vue from "vue";
 import Router from "vue-router";
 import AppHeader from "./layout/AppHeader";
 import AppFooter from "./layout/AppFooter";
-import Components from "./views/Components.vue";
-import Landing from "./views/Landing.vue";
 import Aletheia from "./views/Aletheia.vue";
 import Start from "./views/Start.vue";
-import Login from "./views/Login.vue";
-import Register from "./views/Register.vue";
-import Profile from "./views/Profile.vue";
 import StartFooter from "./layout/StartFooter.vue"
 import StartHeader from "./layout/StartHeader.vue"
 import Account from "./views/Account.vue"
@@ -20,17 +15,9 @@ Vue.use(Router);
 export default new Router({
   linkExactActiveClass: "active",
   routes: [
+    
     {
-      path: "/components",
-      name: "components",
-      components: {
-        header: AppHeader,
-        default: Components,
-        footer: AppFooter
-      }
-    },
-    {
-      path: "/al",
+      path: "/",
       name: "aletheia",
       components: {
         header: AppHeader,
@@ -72,42 +59,6 @@ export default new Router({
         header: StartHeader,
         default: Explore,
         footer: StartFooter
-      }
-    },
-    {
-      path: "/",
-      name: "landing",
-      components: {
-        header: AppHeader,
-        default: Landing,
-        footer: AppFooter
-      }
-    },
-    {
-      path: "/login",
-      name: "login",
-      components: {
-        header: AppHeader,
-        default: Login,
-        footer: AppFooter
-      }
-    },
-    {
-      path: "/register",
-      name: "register",
-      components: {
-        header: AppHeader,
-        default: Register,
-        footer: AppFooter
-      }
-    },
-    {
-      path: "/profile",
-      name: "profile",
-      components: {
-        header: AppHeader,
-        default: Profile,
-        footer: AppFooter
       }
     }
   ],
